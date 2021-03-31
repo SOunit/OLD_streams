0709adea9271c542b0531ee8b0a90ad9e3635fe3
SOUnit <workbookwork7@gmail.com>
2021-03-31 20:23:59 +0900

    added semantic ui

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import StreamEdit from './streams/StreamEdit';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Header!</h1>
+      <BrowserRouter>
+        <div>
+          <Route path='/' exact component={StreamList}></Route>
+          <Route path='/streams/create' component={StreamCreate}></Route>
+          <Route path='/streams/edit' component={StreamEdit}></Route>
+          <Route path='/streams/delete' component={StreamDelete}></Route>
+          <Route path='/streams/show' component={StreamShow}></Route>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
