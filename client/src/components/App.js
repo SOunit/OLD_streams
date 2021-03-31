ba5f56b1b8b78980920b53dcabf7058b8142ad8f
SOUnit <workbookwork7@gmail.com>
2021-04-01 08:02:14 +0900

    added header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import StreamEdit from './streams/StreamEdit';
-import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
 
 const App = () => {
   return (
-    <div>
-      <h1>Header!</h1>
+    <div className='ui container'>
       <BrowserRouter>
         <div>
+          <Header />
           <Route path='/' exact component={StreamList}></Route>
           <Route path='/streams/create' component={StreamCreate}></Route>
           <Route path='/streams/edit' component={StreamEdit}></Route>
