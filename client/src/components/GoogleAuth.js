8487c530e73b3b9c40bf2042dff4482e585e47b1
SOUnit <workbookwork7@gmail.com>
2021-04-01 22:22:37 +0900

    added button

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -25,13 +25,31 @@ class GoogleAuth extends Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
   renderAuthButton = () => {
     if (this.state.isSignedIn === null) {
-      return <div>I dont know if we are signed in</div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>We are signed in!</div>;
+      return (
+        <button onClick={this.onSignOut} className='ui red google button'>
+          <i className='google icon'></i>
+          Sign Out
+        </button>
+      );
     } else {
-      return <div>I am not signed in</div>;
+      return (
+        <button onClick={this.onSignIn} className='ui red google button'>
+          <i className='google icon' />
+          Sign In with Google
+        </button>
+      );
     }
   };
 
