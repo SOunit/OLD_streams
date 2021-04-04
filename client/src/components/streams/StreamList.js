fda43feba5e10106170cb8c1dd3c8bc6b01afb1d
SOUnit <workbookwork7@gmail.com>
2021-04-05 08:15:49 +0900

    added rendering admin button

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -7,15 +7,27 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderAdmin = (stream) => {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className='right floated content'>
+          <button className='ui button primary'>Edit</button>
+          <button className='ui button negative'>Delete</button>
+        </div>
+      );
+    }
+  };
+
   renderList = () => {
     let list = null;
-    list = this.props.streams.map(({ id, title, description }) => {
+    list = this.props.streams.map((stream) => {
       return (
-        <div className='item' key={id}>
+        <div className='item' key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className='large middle aligned icon camera'></i>
           <div className='content'>
-            {title}
-            <div className='description'>{description}</div>
+            {stream.title}
+            <div className='description'>{stream.description}</div>
           </div>
         </div>
       );
@@ -34,7 +46,10 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+  };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
