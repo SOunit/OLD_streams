557b07475814d2e13dfea88753f241f34a4f283b
SOUnit <workbookwork7@gmail.com>
2021-04-06 13:07:03 +0900

    exclude id, userId in redux-form to limit properties to be changed

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,10 +1,16 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
   componentDidMount = () => {
     this.props.fetchStream(this.props.match.params.id);
+  };
+
+  onSubmit = (formValues) => {
+    console.log(formValues, 'on submit');
   };
 
   render = () => {
@@ -13,8 +19,12 @@ class StreamEdit extends React.Component {
     }
     return (
       <div>
-        <div>{this.props.stream.title}</div>
-        <div>{this.props.stream.description}</div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          // to limit property to change, to excluding id, userId
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+          onSubmit={this.onSubmit}
+        />
       </div>
     );
   };
