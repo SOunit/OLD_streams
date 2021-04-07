c69c9200eaf9337f3691f4b78147a195963a565e
SOUnit <workbookwork7@gmail.com>
2021-04-07 12:49:05 +0900

    added fetching data and deleting data to delete page

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,30 +1,55 @@
 import React from 'react';
 import Modal from '../Modal';
 import history from '../../history';
+import { fetchStream, deleteStream } from '../../actions';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class StreamDelete extends React.Component {
+  componentDidMount = () => {
+    this.props.fetchStream(this.props.match.params.id);
+  };
+
   renderActions = () => {
     return (
       <React.Fragment>
-        <button className='ui negative button'>Delete</button>
-        <button className='ui button'>Cancel</button>
+        <button
+          onClick={() => this.props.deleteStream(this.props.match.params.id)}
+          className='ui negative button'
+        >
+          Delete
+        </button>
+        <Link to='/' className='ui button'>
+          Cancel
+        </Link>
       </React.Fragment>
     );
   };
 
+  renderContent = () => {
+    if (!this.props.stream) {
+      return 'Are you sure you want to delete this stream?';
+    }
+
+    return `Are you sure you want to delete the stream with title : ${this.props.stream.title} ?`;
+  };
+
   render = () => {
     return (
-      <div>
-        StreamDelete
-        <Modal
-          title='Delete Stream'
-          content='Are you sure you want to delete this stream?'
-          actions={this.renderActions()}
-          onDismiss={() => history.push('/')}
-        />
-      </div>
+      <Modal
+        title='Delete Stream'
+        content={this.renderContent()}
+        actions={this.renderActions()}
+        onDismiss={() => history.push('/')}
+      />
     );
   };
 }
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProp) => {
+  return { stream: state.streams[ownProp.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
